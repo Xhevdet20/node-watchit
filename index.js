de36768c7b5690c1d9b8e7c149193ce975bc7d84
xhevdet <xhevdet.kryeziu@kujtesa.com>
2020-09-18 16:58:03 +0200

    final node watchit tool

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 const debounce = require('lodash.debounce');
 const chokidar = require('chokidar');
+const program = require('caporal');
+const fs = require('fs');
+const { spawn } = require('child_process');
+const chalk = require('chalk');
+program
+  .version('0.0.1')
+  .argument('[filename]', 'Name of a file to execute')
+  .action(async ({ filename}) => {
+    const name = filename || 'index.js';
 
-chokidar.watch('.')
-  .on('add', () =>  console.log('starting users program'))
-  .on('change', () =>  console.log('file changed'))
-  .on('unlink', () =>  console.log('file unlinked'));
+    try {
+      await fs.promises.access(name);
+    } catch(err){
+      throw new Error(`Could not find the file ${name}`);
+    }
+    
+    let proc;
+
+    const start = debounce(() => {
+      if(proc){
+        proc.kill();
+      }
+      console.log(chalk.blue('>>> Starting process...'));
+     proc = spawn('node', [name], {stdio: 'inherit'});
+    }, 100);
+  
+    chokidar.watch('.')
+      .on('add', start)
+      .on('change', start)
+      .on('unlink', start);
+    });
+
+program.parse(process.argv);
+
